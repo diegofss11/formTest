87053fbae23d3d5043ce965e7d3a1b4a23f0519b
= <diegofss11@gmail.com>
2014-11-19 16:10:34 -0200

    Added Karma and Jasmine

--- GruntFile.js
+++ GruntFile.js
@@ -14,7 +14,7 @@ module.exports = function( grunt ) {
 		},
 		karma: { 
 	        unit: { 
-	            configFile: 'karma.conf.js'	            
+	            configFile: 'config/karma.conf.js'	            
 	        }
 	    },
 	    compass: {
